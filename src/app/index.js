47e6f3ce2978b3e27c87166f3ca618bc4e306724
Parker <parkerproject@gmail.com>
2015-02-24 00:30:50 -0500

    updates

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,13 @@
 'use strict';
 
 angular.module('publishingSystem', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngRoute'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
-;
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/main/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
