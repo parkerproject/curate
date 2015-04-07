235d683e63788315ae5a2ab50571f06dd39ee1d1
Parker <parkerproject@gmail.com>
2015-04-06 22:48:30 -0400

    updates

--- src/app/main/main.services.js
+++ src/app/main/main.services.js
@@ -4,6 +4,7 @@ angular.module('publishingSystem')
         var runRequest = function(options) {
             var searchUrl = 'http://api.dealsbox.co/deals/search';
             searchUrl = searchUrl + '?limit=10&q=' + options.keyword + '&city=' + options.city;
+            console.log(searchUrl);
             if (options.SelectPrice) {
                 searchUrl = searchUrl + '&price=' + options.SelectPrice;
             }
